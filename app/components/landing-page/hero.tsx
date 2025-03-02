2cce84f70f7597b07c52099cbe99185300d5eae9
Anderson Fernandes <anderson.fernandes96@outlook.com>
2025-03-02 16:45:01 -0300

    Criando o componente header

--- app/components/landing-page/hero.tsx
+++ app/components/landing-page/hero.tsx
@@ -6,7 +6,7 @@ import TextInput from "../ui/text-input";
 
 export default function Hero() {
   return (
-    <div className="flex h-screen">
+    <div className="flex h-screen gap-x-10">
       <div className="w-full flex flex-col gap-2 mt-[35vh]">
         <h1 className="text-5xl font-bold text-white leading-[64px]">
           Seus projetos e redes sociais em um único link
--- app/page.tsx
+++ app/page.tsx
@@ -1,15 +1,17 @@
-import Image from "next/image";
+import Header from "./components/landing-page/header";
 import Hero from "./components/landing-page/hero";
 
 export default function Home() {
   return (
-    <h1 className="max-w-7xl mx-auto">
-      <Hero />
-     {/*  <Header />
-      <Hero />
-      <VideoExplanation />
+    <div className="max-w-7xl mx-auto">
+      <Header />
+      <div className="pt-28">
+        <Hero />
+      </div>
+
+      {/* <VideoExplanation />
       <Pricing />
       <FAQ /> */}
-    </h1>
+    </div>
   );
 }
