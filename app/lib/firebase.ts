861707c0cd32fb01c4a0991ab47455ad01fc3fe8
Anderson Fernandes <anderson.fernandes96@outlook.com>
2025-05-06 22:32:26 -0300

    Google config e implementando autenticação

--- app/lib/firebase.ts
+++ app/lib/firebase.ts
@@ -5,7 +5,10 @@ import "server-only";
 
 // Certificado
 
-const decodedKey = Buffer.from(process.env.FIREBASE_PRIVATE_KEY!, "base64").toString("utf-8");
+const decodedKey = Buffer.from(
+  process.env.FIREBASE_PRIVATE_KEY_BASE64!,
+  "base64"
+).toString("utf-8");
 
 export const firebaseCert = cert({
   projectId: process.env.FIREBASE_PROJECT_ID,
